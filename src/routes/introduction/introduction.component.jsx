9eb23efa8efe0eb4464688d6ef6713f50512afb7
fotini <f.batzoni@gmail.com>
2022-10-19 18:57:45 +0300

    Introduction page

---
+++ src/routes/introduction/introduction.component.jsx
@@ -0,0 +1,11 @@
+const Introduction = () => {
+    return (
+        <div>
+            <h1 className='introduction-header'>Are You An NGO Or A Volunteer?</h1>
+            <button className="introduction-btn" type='button'>NGO</button>
+            <button className="introduction-btn" type='button'>Volunteer</button>
+        </div>
+    )
+}
+
+export default Introduction;
