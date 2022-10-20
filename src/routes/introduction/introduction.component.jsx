a154401a1f152cb4778d8e50714ede773424f391
fotini <f.batzoni@gmail.com>
2022-10-20 15:19:37 +0300

    Routing

--- src/routes/introduction/introduction.component.jsx
+++ src/routes/introduction/introduction.component.jsx
@@ -1,13 +1,37 @@
 import './introduction.style.scss'
 import Button from 'react-bootstrap/Button';
+import {useNavigate} from 'react-router-dom'
+
+
 
 const Introduction = () => {
+    const navigate= useNavigate();
+
+    const navigateSignInNgo = () => {
+        navigate('/sign-up/ngo')
+    }
+
+    const navigateSignInVolunteers = () => {
+        navigate('/sign-up/volunteers')
+    }
+    
     return (
         <div className='introduction-div'>
             <h1 className='introduction-header'>Are You An NGO Or A Volunteer?</h1>
             <div className='btn-container'>
-                <Button className="introduction-btn1" variant="dark" size="sm" type='button'>NGO</Button>
-                <Button className="introduction-btn2" variant="dark" size="sm" type='button'>Volunteer</Button>
+                <Button 
+                    className="introduction-btn1" 
+                    variant="dark" size="sm" 
+                    type='button'
+                    onClick={navigateSignInNgo}
+                    >NGO</Button>
+                <Button 
+                    className="introduction-btn2" 
+                    variant="dark" 
+                    size="sm" 
+                    type='button'
+                    onClick={navigateSignInVolunteers}
+                    >Volunteer</Button>
             </div>
         </div>
     )
